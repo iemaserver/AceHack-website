95bc030202f23cec09f2d476498062fa1336efa7
Pulkit Verma <pulkit18012003@gmail.com>
2025-03-10 04:02:47 +0530

    FAQ section with style and function is added

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -121,6 +121,32 @@ function changeClass() {
   }
 }
 
+$(document).ready(function () {
+  $(".accordion-title").click(function (e) {
+    var accordionitem = $(this).attr("data-tab");
+    $("#" + accordionitem)
+      .slideToggle()
+      .parent()
+      .siblings()
+      .find(".accordion-content")
+      .slideUp();
+
+    $(this).toggleClass("active-title");
+    $("#" + accordionitem)
+      .parent()
+      .siblings()
+      .find(".accordion-title")
+      .removeClass("active-title");
+
+    $("i.fa-chevron-down", this).toggleClass("chevron-top");
+    $("#" + accordionitem)
+      .parent()
+      .siblings()
+      .find(".accordion-title i.fa-chevron-down")
+      .removeClass("chevron-top");
+  });
+});
+
 // ====================== Scroll Reveal ======================
 
 // Function to apply ScrollReveal to all elements with a specific class
